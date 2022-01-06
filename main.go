2b0369bcfce1ef1d278d48ddc71fbe2d404de02a
Jang Woo Heo <redheo@hotmail.com>
2022-01-05 19:02:16 -0800

    add gitignore and Makefile. update main.go with comments and refined help output

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ import (
 )
 
 func main() {
+	// default password length if no argument is pass from cli
+	var defaultLength int = 24
+
+	// random seed
 	rand.Seed(time.Now().UnixNano())
 
+	// define characters
 	m := map[string]string{
 		"letters":  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 		"numbers":  "0123456789",
@@ -21,29 +26,43 @@ func main() {
 	}
 
 	app := &cli.App{
-		Name:  "genpass",
-		Usage: "generate random password",
+		// define our cli options here
+		Name:            "genpass",
+		Usage:           "generate random password",
+		UsageText:       "genpass [global option] [password length]",
+		HideHelpCommand: true,
 		Flags: []cli.Flag{
-			&cli.StringFlag{
+			&cli.BoolFlag{
 				Name:    "nospecial",
 				Aliases: []string{"n"},
 				Usage:   "don't include special characters",
 			},
 		},
+		// cli action
 		Action: func(c *cli.Context) error {
+			// convert password length argument to int
 			i, err := strconv.Atoi(c.Args().Get(0))
-
 			if err != nil {
-				i = 24
+				i = defaultLength
 			}
 
+			// remove special from map m if --nospecial option is passed
+			if c.Bool("nospecial") {
+				delete(m, "specials")
+			}
+
+			// create byte slice that will contain password
 			b := make([]byte, i)
 
+			// build our password
 			for i := range b {
-				s := randomKey(m)
+				// pick random key and grab its value
+				s := randomKeyValue(m)
+				// pick random character from the retrieved value from above
 				b[i] = randomCharacter(s)
 			}
 
+			// print our password
 			fmt.Println(string(b))
 
 			return nil
@@ -56,7 +75,7 @@ func main() {
 	}
 }
 
-func randomKey(m map[string]string) string {
+func randomKeyValue(m map[string]string) string {
 	keys := []string{}
 
 	for k := range m {
